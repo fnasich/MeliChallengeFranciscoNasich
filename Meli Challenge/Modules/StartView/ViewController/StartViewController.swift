cd64d535e81c004eefb78644366170973308d75f
Francisco <francisco.nasich@mercadolibre.com>
2022-06-20 13:58:27 -0300

    Refactoring code from StarViewController

--- Meli Challenge/Modules/StartView/ViewController/StartViewController.swift
+++ Meli Challenge/Modules/StartView/ViewController/StartViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 protocol StartViewDelegate {
     func loadData()
-    func dataSearch()
 }
 
 class StartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
@@ -22,17 +21,18 @@ class StartViewController: UIViewController, UITableViewDelegate, UITableViewDat
     var service3 = MultiGetService()
     
     
-    let arreglo = ["Wolkswagen Polo", "Audi R8", "Audi TT", "Citroen C4 Lounge", "Peugeot 208"]
-    var searchArreglo: [String]!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUp()
+    }
+    
+    // MARK: Set Up function
+    func setUp(){
         self.tableView?.delegate = self
         self.tableView?.dataSource = self
         self.searchLabel.delegate = self
         self.tableView.register(UINib(nibName: "StartTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-        searchArreglo = arreglo
         searchLabel.searchTextField.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
         viewModel = StartViewModel(service: service, delegate: self, service2: service2, service3: service3)
     }
@@ -64,21 +64,7 @@ class StartViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
     }
     
-    
-    // MARK: Search Bar
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        searchArreglo = []
-//        if searchText == "" {
-//            searchArreglo = arreglo
-//        } else {
-//            for car in arreglo {
-//                if car.lowercased().contains(searchText.lowercased()) {
-//                    searchArreglo.append(car)
-//                }
-//            }
-//        }
-//    }
-    
+    // MARK: Search Bar Button Clicked
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         SearchTextManager.shared.searchValue = searchLabel.text!.lowercased()
         viewModel?.getCategory()
@@ -86,8 +72,6 @@ class StartViewController: UIViewController, UITableViewDelegate, UITableViewDat
 }
 
 extension StartViewController: StartViewDelegate {
-    func dataSearch() {
-    }
     
     func loadData() {
         tableView.reloadData()
